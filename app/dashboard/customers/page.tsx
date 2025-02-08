0625f13fe27a96ce6b2a0e11083e9f56d1497db4
Gerardo Sanchez <gerardo.sanchez94@gmail.com>
2025-02-08 10:03:27 -0500

    dashboard page and customers page

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Customers: React.FC = () => {
+    return (
+        <div>
+            <h1>Customers Page</h1>
+            <p>Welcome to the Customers page. Here you can view and manage customer information.</p>
+        </div>
+    );
+};
+
+export default Customers;
