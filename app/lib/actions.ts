f4679e32f95f946b7b0c69c96d861291bec6c512
Gerardo Sanchez <gerardo.sanchez94@gmail.com>
2025-02-09 12:38:53 -0500

    update invoice

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -42,3 +42,26 @@ export async function createInvoice(formData: FormData) {
     revalidatePath('/dashboard/invoices');
     redirect('/dashboard/invoices');
 }
+
+const UpdateInvoice = FormSchema.omit({ id: true, date: true });
+
+export async function updateInvoice(id: string, formData: FormData) {
+    const { customerId, amount, status } = UpdateInvoice.parse({
+        customerId: formData.get('customerId'),
+        amount: formData.get('amount'),
+        status: formData.get('status'),
+    });
+
+    const amountInCents = amount * 100;
+
+    await sql`
+        UPDATE invoices
+        SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+        WHERE id = ${id}
+    `
+
+    // Calling revalidatePath to clear the client cache and make a new server request.
+    revalidatePath('/dashboard/invoices');
+    // Navigation
+    redirect('/dashboard/invoices');
+}
