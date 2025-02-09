24641ef03759cae7b638d12281a774d63ee74b1d
Gerardo Sanchez <gerardo.sanchez94@gmail.com>
2025-02-09 12:19:29 -0500

    create entity invoice

---
+++ app/lib/actions.ts
@@ -0,0 +1,44 @@
+'use server'; // explicit server declaration
+
+import { z } from 'zod';
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
+import postgres from 'postgres';
+
+const sql = postgres(process.env.POSTGRES_URL!, { });
+
+const FormSchema = z.object({
+    id: z.string(),
+    customerId: z.string(),
+    amount: z.coerce.number(),
+    status: z.enum(['pending', 'paid']),
+    date: z.string(),
+});
+
+const CreateInvoice = FormSchema.omit({ id: true, date: true });
+
+/**
+ * These server functions can then be imported and used in Client and Server components.
+ * Any functions included in this file that are not used will be automatically removed from the final application bundle.
+ */
+
+export async function createInvoice(formData: FormData) {
+
+    const { customerId, amount, status } = CreateInvoice.parse({
+        customerId: formData.get('customerId'),
+        amount: formData.get('amount'),
+        status: formData.get('status'),
+    });
+    const amountInCents = amount * 100;
+    const date = new Date().toISOString().split('T')[0];
+
+    await sql`
+        INSERT INTO invoices (customer_id, amount, status, date)
+        VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+    `
+    /**
+     * This will clear the cache and trigger a new request to the server
+     */
+    revalidatePath('/dashboard/invoices');
+    redirect('/dashboard/invoices');
+}
