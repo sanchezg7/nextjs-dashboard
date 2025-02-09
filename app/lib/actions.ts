b2d096cfde2bba380d1b147f2e963eddbc250318
Gerardo Sanchez <gerardo.sanchez94@gmail.com>
2025-02-09 12:43:08 -0500

    deleting

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -65,3 +65,8 @@ export async function updateInvoice(id: string, formData: FormData) {
     // Navigation
     redirect('/dashboard/invoices');
 }
+
+export async function deleteInvoice(id: string) {
+    await sql`DELETE FROM invoices WHERE id = ${id}`;
+    revalidatePath('/dashboard/invoices');
+}
